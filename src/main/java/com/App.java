58ced697411f5a1caa041b06352757251296eb3a
anshul-Khoros <anshul.jain@khoros.com>
2020-08-11 18:56:29 +0530

    Added Rest Api's

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,12 +1,24 @@
 package com;
 
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Configuration;
+import com.yammer.dropwizard.config.Environment;
+import resources.TweetUtility;
 import twitter4j.TwitterException;
 
 
-public class App {
-    public static void main(String[] args) throws TwitterException {
-        TweetUtility tUtility = new TweetUtility();
-        tUtility.postTweet("Hello new");
-        tUtility.showTimeline();
+public class App extends Service<Configuration> {
+    public static void main(String[] args) throws Exception {
+        new App().run(args);
+    }
+
+
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+
+    }
+
+    public void run(Configuration configuration, Environment environment) throws Exception {
+        environment.addResource(new TweetUtility());
     }
 }
---
+++ src/main/java/resources/TweetUtility.java
@@ -0,0 +1,65 @@
+package resources;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+
+@Path("/api/1.0/twitter")
+public class TweetUtility {
+
+    Twitter twitter;
+
+    public TweetUtility(){
+        String consumerKey, consumerSecret, accessToken, accessTokenSecret;
+        consumerKey = System.getProperty("ConsumerKey");
+        consumerSecret = System.getProperty("ConsumerSecret");
+        accessToken = System.getProperty("AccessToken");
+        accessTokenSecret = System.getProperty("AccessTokenSecret");
+
+
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        this.twitter = tf.getInstance();
+
+    }
+
+    @GET
+    @Path("/timeline")
+    @Produces("application/json")
+    public Response showTimeline() throws TwitterException {
+        try {
+            List<Status> statuses = this.twitter.getHomeTimeline();
+            return Response.status(Response.Status.OK).entity(statuses).build();
+        }
+        catch (RuntimeException ex) {
+            return Response.serverError().entity(ex.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/tweet/{tweet}")
+    public Response postTweet(@PathParam("tweet") String tweet) throws TwitterException {
+        if(tweet == null) {
+            return Response.serverError().entity("No tweet provided").build();
+        }
+        try {
+            this.twitter.updateStatus(tweet);
+            return Response.status(Response.Status.OK).build();
+        }
+        catch (RuntimeException ex) {
+            return Response.serverError().entity(ex.getMessage()).build();
+        }
+    }
+}
