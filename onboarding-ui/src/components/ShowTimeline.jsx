28a0f4adf2b827c0a0bd77fbc7c3c1fef54fc3fc
anshul-Khoros <anshul.jain@khoros.com>
2020-09-01 17:44:33 +0530

    user timeline tab

--- onboarding-ui/src/components/ShowTimeline.jsx
+++ onboarding-ui/src/components/ShowTimeline.jsx
@@ -4,38 +4,98 @@ import { getTimeline } from '../apis/apiRequest'
 
 
 
-class ShowTimeline extends React.Component{
-    constructor(props){
+class ShowTimeline extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            tweets:[]
+        this.state = {
+            tweets: [],
+            userTweets: [],
+            selectedTab: 'home'
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.fetchTweets();
     }
 
-    fetchTweets(){
+    fetchTweets() {
         const url = 'http://localhost:8080/api/1.0/twitter/timeline';
         // axios api call to fetch timline
         getTimeline()
-        .then((res)=>{
-           this.setState({
-                tweets:res.data
-           })
-        })
-        .catch(e=>{
-            console.log("error", e);
-        })
+            .then((res) => {
+                this.setState({
+                    tweets: res.data
+                })
+            })
+            .catch(e => {
+                console.log("error", e);
+            })
     }
 
-    render(){
-        return(
+    handleTab(selectedTab) {
+        switch (selectedTab) {
+            case 'home':
+                this.fetchTweets();
+                break;
+            case 'user_timeline':
+                break;
+            default:
+                return null;
+        }
+        this.setState({
+            selectedTab
+        });
+    }
+
+    getTabs() {
+        const tabs = [{ label: "Home", value: 'home' }, { label: "User Timeline", value: 'user_timeline' }];
+        let { selectedTab } = this.state;
+
+        return (tabs.map((eachTab, tabKey) => {
+            return (
+                <div
+                    key={eachTab.value}
+                    className={`tab ${eachTab.value === selectedTab ? 'active-tab' : ''}`}
+                    onClick={() => { this.handleTab(eachTab.value) }}
+                >
+                    {eachTab.label}
+                </div>
+            )
+        })
+
+        )
+    }
+
+    getTabsContent() {
+        let { selectedTab, userTweets, tweets } = this.state;
+        switch (selectedTab) {
+            case 'home':
+                return (
+                    <Tweets
+                        tweets={tweets}
+                    />
+                )
+            case 'user_timeline':
+                return (
+                    <Tweets tweets={userTweets} />
+                )
+            default:
+                return null;
+        }
+    }
+
+    render() {
+
+        return (
             <div>
-               <Tweets
-                tweets={this.state.tweets}
-               />
+                <div className="tab-container">
+                    {this.getTabs()}
+                </div>
+                <div className="tab-content">
+                    {this.getTabsContent()}
+                </div>
+
+
             </div>
         )
     }
