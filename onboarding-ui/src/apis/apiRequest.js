a79706ef49d64381fd47bf0cb85fbd03d9882da0
anshul-Khoros <anshul.jain@khoros.com>
2020-09-02 10:20:12 +0530

    Filter timeline

--- onboarding-ui/src/apis/apiRequest.js
+++ onboarding-ui/src/apis/apiRequest.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const getTimeline = () => {
-    const getTimelineUrl = 'http://localhost:8080/api/1.0/twitter/timeline';
+const getTimeline = (url) => {
+    let getTimelineUrl = 'http://localhost:8080/api/1.0/twitter/timeline';
+    if(url){
+        getTimelineUrl = url;
+    }
     return axios(getTimelineUrl)
 }
 
--- onboarding-ui/src/components/ShowTimeline.jsx
+++ onboarding-ui/src/components/ShowTimeline.jsx
@@ -10,7 +10,9 @@ class ShowTimeline extends React.Component {
         this.state = {
             tweets: [],
             userTweets: [],
-            selectedTab: 'home'
+            selectedTab: 'home',
+            filterText: '',
+            filterClicked: false
         }
     }
 
@@ -19,9 +21,14 @@ class ShowTimeline extends React.Component {
     }
 
     fetchTweets() {
-        const url = 'http://localhost:8080/api/1.0/twitter/timeline';
+        let { filterClicked, filterText } = this.state;
+        let url = 'http://localhost:8080/api/1.0/twitter/timeline';
+        if (filterText) {
+            url = 'http://localhost:8080/api/1.0/twitter/timeline/filter/' + filterText;
+        }
+
         // axios api call to fetch timline
-        getTimeline()
+        getTimeline(url)
             .then((res) => {
                 this.setState({
                     tweets: res.data
@@ -84,17 +91,42 @@ class ShowTimeline extends React.Component {
         }
     }
 
+    handleInputChange(filterText) { this.setState({ filterText }) }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let { selectedTab } = this.state;
+        switch (selectedTab) {
+            case 'home':
+                this.fetchTweets();
+                break;
+            case 'user_timeline':
+                break;
+            default:
+                return null;
+        }
+
+    }
+
     render() {
+        let { filterText } = this.state;
 
         return (
-            <div>
+            <div >
+                <div className="timeline-actions">
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input class="input" type="text" placeholder="Filter tweets" value={filterText}
+                        onChange={(e) => this.handleInputChange(e.target.value)}  />
+                    <input class="submit" type="submit" value="Apply Filter" />
+                </form>
                 <div className="tab-container">
                     {this.getTabs()}
                 </div>
-                <div className="tab-content">
-                    {this.getTabsContent()}
-                </div>
 
+               </div>
+               <div className="tab-content">
+                                   {this.getTabsContent()}
+                               </div>
 
             </div>
         )
